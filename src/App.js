1805e91d26634c03405d002ebd1e929c4f0c11d7
Durgesh Upadhyay <udurgesh6@gmail.com>
2020-11-17 11:09:39 +0530

    initial-commit completing my navbar-component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+//Components
+import Mynavbar from "./components/my-navbar/mynavbar.component";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Mynavbar/>
+      <h1>My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
